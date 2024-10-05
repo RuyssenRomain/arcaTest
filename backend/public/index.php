2c65cdde7a90b545a3b52c5b4bd3467beabb4bc1
Ruyssen Romain <ruyssensud@gmail.com>
2024-10-06 00:14:13 +0200

    - Déplacement des versions des images Docker et des numéros de ports vers le fichier `.env` pour faciliter la configuration. - Mise en place des fichiers `.env.dev` et `.env.prod` pour les variables spécifiques aux environnements de développement et de production. - Mise à jour des `Dockerfile` pour utiliser les variables d'argument (`ARG`) et accepter les versions spécifiées.

---
+++ backend/public/index.php
@@ -0,0 +1,4 @@
+<?php
+# test de fonctionnement sur le port 8080 pour voir si le service PHP fonctionne
+echo "Le service PHP fonctionne correctement !";
+?>
--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,24 +1,30 @@
 CREATE DATABASE IF NOT EXISTS aracadizoo_db;
 USE aracadizoo_db;
 
--- Créer l'utilisateur 'arcadia' avec un mot de passe
-CREATE USER 'arcadiaUser'@'%' IDENTIFIED BY 'Arcadia@180927';
+-- Supprimer l'utilisateur s'il existe déjà
+DROP USER IF EXISTS 'arcadiaUser'@'%';
 
--- Accorder tous les privilèges à 'arcadia'
-GRANT ALL PRIVILEGES ON *.* TO 'arcadia'@'%' WITH GRANT OPTION;
+DROP DATABASE IF EXISTS arcatest;
+
+
+-- Créer l'utilisateur 'arcadiaUser' avec un mot de passe
+CREATE USER 'arcadiaUser'@'%' IDENTIFIED BY 'Password';
+
+-- Accorder tous les privilèges à 'arcadiaUser' sur la base de données spécifique
+GRANT ALL PRIVILEGES ON aracadizoo_db.* TO 'arcadiaUser'@'%' WITH GRANT OPTION;
 
 -- Appliquer les changements
 FLUSH PRIVILEGES;
 
--- Création des tables
-CREATE TABLE habitats (
+-- Création des tables (avec IF NOT EXISTS)
+CREATE TABLE IF NOT EXISTS habitats (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(255) NOT NULL,
   description TEXT,
   image VARCHAR(255)
 );
 
-CREATE TABLE animaux (
+CREATE TABLE IF NOT EXISTS animaux (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(255) NOT NULL,
   description TEXT,
@@ -28,19 +34,19 @@ CREATE TABLE animaux (
   FOREIGN KEY (habitat_id) REFERENCES habitats(id)
 );
 
-CREATE TABLE admins (
+CREATE TABLE IF NOT EXISTS admins (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
--- Insertion des données de test
-INSERT INTO habitats (nom, description) VALUES
+-- Insertion des données de test (avec INSERT IGNORE)
+INSERT IGNORE INTO habitats (nom, description) VALUES
 ('Savane', 'Habitat pour les lions'),
 ('Jungle', 'Habitat pour les tigres'),
 ('Océan', 'Habitat pour les dauphins');
 
-INSERT INTO animaux (nom, description, habitat_id) VALUES
+INSERT IGNORE INTO animaux (nom, description, habitat_id) VALUES
 ('Lion', 'Le roi de la savane', 1),
 ('Tigre', 'Grand félin de la jungle', 2),
 ('Dauphin', 'Mammifère marin intelligent', 3);
